b5492106b652e80812621c4521ed3034c6987d6c
pbp965 <pratikpatel05@outlook.com>
2026-04-03 17:06:52 -0400

    added nav.php, updated register, first step of login.php

--- public_html/project/login.php
+++ public_html/project/login.php
@@ -3,7 +3,7 @@ require(__DIR__ . "/../../partials/nav.php");
 ?>
 
 
-<h3>Register</h3>
+<h3>Login</h3>
 <form onsubmit="return validate(this)" method="POST">
     <div>
         <label for="email">Email</label>
@@ -13,11 +13,7 @@ require(__DIR__ . "/../../partials/nav.php");
         <label for="pw">Password</label>
         <input type="password" id="pw" name="password" required minlength="8" />
     </div>
-    <div>
-        <label for="confirm">Confirm</label>
-        <input type="password" name="confirm" required minlength="8" />
-    </div>
-    <input type="submit" value="Register" />
+    <input type="submit" value="Login" />
 </form>
 <script>
     function validate(form) {
@@ -29,11 +25,10 @@ require(__DIR__ . "/../../partials/nav.php");
 </script>
 <?php
 //TODO 2: add PHP Code
-if (isset($_POST["email"], $_POST["password"], $_POST["confirm"])) {
+if (isset($_POST["email"], $_POST["password"])) {
 
     $email = se($_POST, "email", "", false);
     $password = se($_POST, "password", "", false);
-    $confirm = se($_POST, "confirm", "", false);
     // TODO 3: validate/use
     $hasError = false;
 
@@ -53,37 +48,16 @@ if (isset($_POST["email"], $_POST["password"], $_POST["confirm"])) {
         $hasError = true;
     }
 
-    if (empty($confirm)) {
-        echo "Confirm password must not be empty<br>";
-        $hasError = true;
-    }
-
     if (strlen($password) < 8) {
         echo "Password too short<br>";
-        $hasError = true;
-    }
-
-    if ($password !== $confirm) {
-        echo "Passwords must match<br>";
         $hasError = true;
     }
 
     if (!$hasError){
         // comment out or delete the "success" echo
         // echo "Success<br>";
-        // TODO 4: Hash password before storing
-        $hashed_password = password_hash($password, PASSWORD_BCRYPT);   
-        $db = getDB(); // available due to the `require()` of `functions.php` 
-        // Code for inserting user data into the database
-        $stmt = $db->prepare("INSERT INTO Users (email, password) VALUES (:email, :password)"); 
-        try{
-            $stmt->execute([':email' => $email, ':password' => $hashed_password]); 
-            echo "Successfully registered!";
-        }
-        catch(Exception $e){
-            echo "There was an error registering<br>"; // user-friendly message
-            error_log("Registration Error: " . var_export($e, true)); // log the technical error for debugging
-        }
+        // TODO 4: Check password and fetch user
+
     }
 }
 ?>
